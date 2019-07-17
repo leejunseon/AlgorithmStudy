7f2610de69b2b23ea434d570c724adf954de844d
Unknown <ljs921026@gmail.com>
2019-07-17 17:19:22 +0900

    2019-07-17
    문제 수정

--- Simulation/[SW_expertAcademy]4340. [연습문제] 파이프 연결.java
+++ Simulation/[SW_expertAcademy]4340. [연습문제] 파이프 연결.java
@@ -1,60 +1,57 @@
-/* https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWL6LhM6A60DFAUY */
-//라이브러리사용이 금지되어있어서 제출은 못했지만 문제상의 테스트케이스는 통과
+/* https://swexpertacademy.com/main/code/problem/problemDetail.do */
+//시간초과발생. 탐색 과정에서 dp 적용방법 생각해볼 것.
 
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 	public static int n;
 	public static int[][] map;
 	public static int[][] check;
-	public static List<Integer> up;
-	public static List<Integer> down;
-	public static List<Integer> left;
-	public static List<Integer> right;
-	public static List<Integer> curved;
-	public static List<Integer> straighten;
+	public static int[] up;
+	public static int[] down;
+	public static int[] left;
+	public static int[] right;
+	public static int[] curved;
+	public static int[] straighten;
 	public static int[] dy= {-1,1,0,0};
 	public static int[] dx= {0,0,-1,1};
 	
 	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		Scanner sc=new Scanner(System.in);
 		
-		up=new ArrayList<Integer>();
-		down=new ArrayList<Integer>();
-		left=new ArrayList<Integer>();
-		right=new ArrayList<Integer>();
-		curved=new ArrayList<Integer>();
-		straighten=new ArrayList<Integer>();
-		up.add(2);
-		up.add(3);
-		up.add(4);
-		down.add(2);
-		down.add(5);
-		down.add(6);
-		left.add(1);
-		left.add(3);
-		left.add(6);
-		right.add(1);
-		right.add(4);
-		right.add(5);
+		up=new int[3];
+		down=new int[3];
+		left=new int[3];
+		right=new int[3];
+		curved=new int[4];
+		straighten=new int[2];
+		up[0]=2;
+		up[1]=3;
+		up[2]=4;
+		down[0]=2;
+		down[1]=5;
+		down[2]=6;
+		left[0]=1;
+		left[1]=3;
+		left[2]=6;
+		right[0]=1;
+		right[1]=4;
+		right[2]=5;
 		for(int i=1;i<=2;i++)
-			straighten.add(i);
+			straighten[i-1]=i;
 		for(int i=3;i<=6;i++)
-			curved.add(i);
+			curved[i-3]=i;
 		
 		
-		int t=Integer.parseInt(br.readLine());
+		int t=sc.nextInt();
 		for(int test=1;test<=t;test++) {
 			
-			n=Integer.parseInt(br.readLine());
+			n=sc.nextInt();
 			map=new int[n][n];
 			check=new int[n][n];
 			for(int i=0;i<n;i++) {
-				st=new StringTokenizer(br.readLine());
 				for(int j=0;j<n;j++) {
-					map[i][j]=Integer.parseInt(st.nextToken());
+					map[i][j]=sc.nextInt();
 				}
 			}
 			
@@ -69,20 +66,6 @@ public class Solution {
 	
 	//최적길이를 반환
 	public static int Optimal(int in,Pair start,int num,int[][] map,int[][] check) {
-		//기저사항
-		if(start.y==n-1&&start.x==n-1) {
-			if(in==0) {
-				if(curved.contains(map[start.y][start.x])) {
-					return num;
-				}
-			}
-			if(in==2) {
-				if(straighten.contains(map[start.y][start.x])) {
-					return num;
-				}
-			}
-		}
-		
 		int result=987654321;
 		int[][] clone=new int[n][n];
 		int[][] chclone=new int[n][n];
@@ -92,6 +75,20 @@ public class Solution {
 		}
 		chclone[start.y][start.x]=1;
 		
+		//기저사항
+		if(start.y==n-1&&start.x==n-1) {
+			if(in==0) {
+				if(Contains(curved,map[start.y][start.x])) {
+					return num;
+				}
+			}
+			if(in==2) {
+				if(Contains(straighten,map[start.y][start.x])) {
+					return num;
+				}
+			}
+		}
+			
 		for(int i=0;i<4;i++) {
 			if(i==in)
 				continue;
@@ -102,17 +99,19 @@ public class Solution {
 				case 0://위에 
 					switch(in) {
 					case 1://입구아래쪽
-						if(straighten.contains(clone[start.y][start.x]))
+						if(Contains(straighten,clone[start.y][start.x])) {
+							clone[start.y][start.x]=2;
 							result=Math.min(result, Optimal(1,new Pair(nexty,nextx),num+1,clone,chclone));
+						}
 						break;
 					case 2://입구왼쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=5;
 							result=Math.min(result, Optimal(1,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 3://입구오른쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=6;
 							result=Math.min(result, Optimal(1,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
@@ -122,17 +121,19 @@ public class Solution {
 				case 1://아래에
 					switch(in) {
 					case 0://입구위쪽
-						if(straighten.contains(clone[start.y][start.x]))
+						if(Contains(straighten,clone[start.y][start.x])) {
+							clone[start.y][start.x]=2;
 							result=Math.min(result, Optimal(0,new Pair(nexty,nextx),num+1,clone,chclone));
+						}
 						break;
 					case 2://입구왼쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=4;
 							result=Math.min(result, Optimal(0,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 3://입구오른쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=3;
 							result=Math.min(result, Optimal(0,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
@@ -142,44 +143,60 @@ public class Solution {
 				case 2://왼쪽에
 					switch(in) {
 					case 0://입구위쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=5;
 							result=Math.min(result, Optimal(3,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 1://입구아래쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=4;
 							result=Math.min(result, Optimal(3,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 3://입구오른쪽
-						if(straighten.contains(clone[start.y][start.x]))
+						if(Contains(straighten,clone[start.y][start.x])) {
+							clone[start.y][start.x]=1;
 							result=Math.min(result, Optimal(3,new Pair(nexty,nextx),num+1,clone,chclone));
+						}
 						break;
 					}
 					break;
 				case 3://오른쪽에
 					switch(in) {
 					case 0://입구위쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=6;
 							result=Math.min(result, Optimal(2,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 1://입구아래쪽
-						if(curved.contains(clone[start.y][start.x])) {
+						if(Contains(curved,clone[start.y][start.x])) {
 							clone[start.y][start.x]=3;
 							result=Math.min(result, Optimal(2,new Pair(nexty,nextx),num+1,clone,chclone));
 						}
 						break;
 					case 2://입구왼쪽
-						if(straighten.contains(clone[start.y][start.x]))
+						if(Contains(straighten,clone[start.y][start.x])) {
+							clone[start.y][start.x]=1;
 							result=Math.min(result, Optimal(2,new Pair(nexty,nextx),num+1,clone,chclone));
+						}
 						break;
 					}
 					break;
 				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public static boolean Contains(int[] contain,int number) {
+		boolean result=false;
+		
+		for(int i=0;i<contain.length;i++) {
+			if(contain[i]==number) {
+				result=true;
 			}
 		}
 		
